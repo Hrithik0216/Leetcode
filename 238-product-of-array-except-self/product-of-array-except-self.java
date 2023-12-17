8bbf62719a06d13d6a329558b5cb2e4d2fd4dad8
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2023-12-17 19:11:53 +0530

    Time: 2 ms (64.35%) | Memory: 53.6 MB (22.92%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
-        int n = nums.length;
-        int[] result = new int[n];
-        
-        // Calculate product of elements to the left of each index
-        int leftProduct = 1;
-        for (int i = 0; i < n; i++) {
-            result[i] = leftProduct;
-            leftProduct *= nums[i];
+        int left [] = new int [nums.length];
+        int right [] = new int [nums.length];
+        int result [] = new int [nums.length];
+
+        left[0]=1;
+        for(int i=1;i<nums.length;i++){
+            left[i]=left[i-1]*nums[i-1];
         }
 
-        // Calculate product of elements to the right of each index and multiply with the result array
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= rightProduct;
-            rightProduct *= nums[i];
+        right[nums.length-1]=1;
+        for(int i=nums.length-2;i>=0;i--){
+            right[i]=right[i+1]*nums[i+1];
         }
 
+        for(int i=0;i<nums.length;i++){
+            result[i]= right[i]*left[i];
+        }
         return result;
     }
 }
