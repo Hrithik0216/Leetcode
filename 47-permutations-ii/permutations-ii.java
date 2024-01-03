781bafff9f04116472d247f9c03e630d2740c793
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2024-01-03 09:50:27 +0530

    Time: 859 ms (5.01%) | Memory: 44.8 MB (28.59%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,34 +1,25 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);  // Sort the input array to handle duplicates
-        backtrack(nums, result, new ArrayList<>(), new boolean[nums.length]);
+        List<List<Integer>> result = new ArrayList();
+        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
         return result;
     }
-
-    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> tempList, boolean[] used) {
-        if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int [] nums, boolean[] used){
+        if(temp.size()==nums.length && !result.contains(temp)){
+            result.add(new ArrayList<>(temp));
             return;
         }
+        for(int i=0;i<nums.length;i++){
+            if(!used[i]){
+            used[i] =true;
+            temp.add(nums[i]);
+            
+            backtrack(result, temp, nums, used);
 
-        for (int i = 0; i < nums.length; i++) {
-            // Skip duplicate elements
-            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
-                continue;
-            }
-
-            tempList.add(nums[i]);
-            used[i] = true;
-
-            backtrack(nums, result, tempList, used);
-
-            tempList.remove(tempList.size() - 1);
-            used[i] = false;
+            used[i]=false;
+            temp.remove(temp.size()-1); 
         }
-    }
+        }
+        
+    } 
 }
