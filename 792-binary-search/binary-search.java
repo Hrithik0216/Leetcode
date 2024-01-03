e9ade96a73f0739eedf4809cb1f48cec5c888941
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2024-01-03 09:21:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (36.10%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int low = 0;
+    int high = nums.length-1;
+      
+       return binaryRecursion(low, high, target, nums);
+    }
+    private int binaryRecursion(int low, int high, int target, int [] nums){
+        if(low<=high){
+            int mid = low+(high-low)/2;
+        
+         
+        if(target==nums[mid]){
+            return mid;
+        }
+        if(target<nums[mid]){
+            return binaryRecursion(low, mid-1, target, nums);
+        }else {
+            return binaryRecursion(mid+1, high,target, nums);
+        }
+        }
+        return -1;
+    }
+}
